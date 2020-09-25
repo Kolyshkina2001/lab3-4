259400bf89258af63505356dfc0cf7d5fb0593b3
KARPOVTOP <coolxex86@gmail.com>
2020-09-25 10:50:39 +0300

    Initial commit

---
+++ src/ru/mirea/Yana/lab3/Movable.java
@@ -0,0 +1,8 @@
+package ru.mirea.Yana.lab3;
+
+interface Movable {
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRight();
+}
---
+++ src/ru/mirea/Yana/lab3/main.java
@@ -0,0 +1,8 @@
+package ru.mirea.Yana.lab3;
+
+public class main {
+    public static void main(String[] args) {
+        MovableCircle circle1 = new MovableCircle(1, 2, 5, 6, 10);
+        System.out.println(circle1.center.toString());
+    }
+}
